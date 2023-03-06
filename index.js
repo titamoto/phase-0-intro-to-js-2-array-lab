bf3b302048b78f729b04c623159a3a700f97aa1d
Aleksandr Savchenko <titamoto@gmail.com>
2023-03-06 08:50:07 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newcats = [...cats, name];
+    return newcats;
+}
+
+function prependCat(name) {
+    const newcats = [name, ...cats];
+    return newcats;
+}
+
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+
